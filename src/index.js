5888559ac6e1ff6a42c5c416df93214b754c1517
jaw <62795911+JacobWestby@users.noreply.github.com>
2022-12-30 09:23:44 +0100

    testing some stuff

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-const pizzaImage = document.querySelector('.pizza__image');
+const pizzaImagePhone = document.querySelector('.pizza__image-phone');
+const pizzaImageDesktop = document.querySelector('.pizza__image-desktop');
+const overlay = document.querySelector('.pizza__image-overlay');
 
 const setPizaImage = () => {
     if (window.innerWidth < 768) {
-        pizzaImage.style.backgroundImage = "url('../src/images/Banner_Pizza-removebg.png')";
+        pizzaImagePhone.style.backgroundImage = "url('../src/images/Banner_Pizza-removebg.png')";
     } else {
-        pizzaImage.style.backgroundImage = "url('../src/images/Banner_Pizza_cropped.jpg')";
-        pizzaImage.style.backgroundSize = "cover";
-        pizzaImage.style.position = "absolute";
-        pizzaImage.style.top = "0";
-        // pizzaImage.style.backgroundPosition = "-650px 0";
+        pizzaImageDesktop.style.backgroundImage = "url('../src/images/Banner_Pizza_cropped.jpg')";
+        pizzaImageDesktop.style.backgroundSize = "cover";
+        pizzaImageDesktop.style.position = "absolute";
+        pizzaImageDesktop.style.top = "0";
+        pizzaImageDesktop.style.background
+
+        overlay.style.backgroundColor = "rgba(62, 40, 40, 0.3)";
     }
 }
 
